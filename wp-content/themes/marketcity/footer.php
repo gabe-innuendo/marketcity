49350ccc0ae7b677be83c41fc14c0abba5a5e03d
Gabriel Grieve <gabe@innuendodigital.com.au>
2017-09-13 11:08:21 +1000

    New footer added and styled

--- wp-content/themes/marketcity/footer.php
+++ wp-content/themes/marketcity/footer.php
@@ -1,4 +1,43 @@
-
+<footer class="main-footer">
+	<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 col-xs-12 logo-section">
+            	<div class="footer-logo-container">
+            		<img src="<?php get_image_url(); ?>/market-city-logo.svg" alt="Market City Logo"/>
+            	</div>
+            	<h5>Market City</h5>
+            	<address>
+            	9-13 Hay Street <br />
+            	Haymarket NSW 2000
+            	</address>
+            </div>
+            <div class="col-md-3 col-xs-12">
+            	<h5>Post Address</h5>
+            	<address>
+            	Market City Centre Management<br/ >
+            	PO Box K23<br />
+            	Haymarket NSW 1240
+            	</address>
+            </div>
+            <div class="col-md-3 col-xs-12">
+            	<h5>Contact us</h5>
+            	<p>
+            		<strong>P: </strong>(02) 9288 8900<br />
+            		<strong>F: </strong>(02) 9212 1302<br />
+            		<strong>E: </strong>info@marketcity.com.au
+            	</p>
+            </div>
+            <div class="col-md-3 col-xs-12 social-section">
+            	<h5>Social Media</h5>
+            	<p>Get involved with our social media communities for exclusive news, prizes and offers!</p>
+            	<section class="social-media-buttons">
+		            <a target="_blank" class="social-button white" href="https://www.facebook.com/marketcity/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		            <a target="_blank" class="social-button white" href="https://www.instagram.com/marketcity/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		        </section>
+            </div>
+        </div>
+    </div>
+</footer>
 <?php wp_footer(); ?> 
 </body>
 </html>
